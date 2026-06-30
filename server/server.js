7405d860af2d7667f4c097aa1d4dbe77b5a7e467
nagamanisegalla <nagamanisegalla@gmail.com>
2026-06-30 14:35:53 +0530

    Setup Express server and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Employee Management API");
+});
+
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
+});
