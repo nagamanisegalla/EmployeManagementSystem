ec377fb4bceca4ea2a4baf716756e9f16397bddb
nagamanisegalla <nagamanisegalla@gmail.com>
2026-06-30 17:41:48 +0530

    Implemented React login page and authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,90 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+
+import api from "../services/api";
+import { AuthContext } from "../context/AuthContext";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]:
+        e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response =
+        await api.post(
+          "/auth/login",
+          formData
+        );
+
+      login(
+        response.data.token,
+        response.data.user
+      );
+
+      toast.success(
+        "Login Successful"
+      );
+
+      setTimeout(() => {
+        navigate("/dashboard");
+      }, 1000);
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+          "Login Failed"
+      );
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <ToastContainer />
+
+      <form
+        className="login-form"
+        onSubmit={handleSubmit}
+      >
+        <h2>Employee Management System</h2>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
