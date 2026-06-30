b4ed38b4532ba55721ec288386a728926217274a
nagamanisegalla <nagamanisegalla@gmail.com>
2026-06-30 14:51:11 +0530

    Implemented authentication controller with JWT

--- server/server.js
+++ server/server.js
@@ -5,23 +5,28 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
+// Connect Database
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Default Route
 app.get("/", (req, res) => {
-  res.send("Employee Management API");
+  res.send("Employee Management API Running...");
 });
 
-const PORT =
-  process.env.PORT || 5000;
+// Server
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+  console.log(`🚀 Server running on port ${PORT}`);
 });
